f090352a6875dbbdd1d6a63fb95d7ed542d0b85a
Vacietis <t.vacietis@gmail.com>
2016-02-09 17:50:08 +0200

    scroll directivas labojums

--- app/views/spaceHistory/spaceHistory.directive.js
+++ app/views/spaceHistory/spaceHistory.directive.js
@@ -3,16 +3,16 @@
 
     angular
         .module('spaceHistory')
-        .directive('scrollWatcher', scrollWatcher);
+        .directive('scroll-watcher', scrollWatcher);
 
     function scrollWatcher(){
         return {
             restrict: 'A',
             link: function(scope,elem,attrs){
-                elem.on('scroll', function(evt){
-                    $timeout(function() {
-                         console.log(evt.scrollLeft);
-                    },0);
+                elem.on('scroll', function(){
+                    
+                    console.log(this.scrollLeft);
+                  
                   
                 });
             }
